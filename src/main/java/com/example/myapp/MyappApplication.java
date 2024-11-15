d5a2c222a814f4787757638c8364d0e7fff068bb
Tarun Ramani <tarunpatelr@gmail.com>
2024-11-14 19:14:53 -0500

    update props

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -2,12 +2,18 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyappApplication {
+public class MyappApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyappApplication.class);
+	}
 }
